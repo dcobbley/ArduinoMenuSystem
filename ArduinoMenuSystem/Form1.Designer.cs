fe45868c9934af1ad3388c9695dc8aadb4d332c5
dcobbley <djcobbles@gmail.com>
2014-12-09 23:57:05 -0800

    added buttons and "LCD screen" simulation

--- ArduinoMenuSystem/Form1.Designer.cs
+++ ArduinoMenuSystem/Form1.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_up = new System.Windows.Forms.Button();
+            this.btn_down = new System.Windows.Forms.Button();
+            this.btn_left = new System.Windows.Forms.Button();
+            this.btn_right = new System.Windows.Forms.Button();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.tb_top = new System.Windows.Forms.TextBox();
+            this.tb_bottom = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_up
+            // 
+            this.btn_up.Location = new System.Drawing.Point(101, 217);
+            this.btn_up.Name = "btn_up";
+            this.btn_up.Size = new System.Drawing.Size(75, 23);
+            this.btn_up.TabIndex = 0;
+            this.btn_up.Text = "UP";
+            this.btn_up.UseVisualStyleBackColor = true;
+            this.btn_up.Click += new System.EventHandler(this.btn_up_Click);
+            // 
+            // btn_down
+            // 
+            this.btn_down.Location = new System.Drawing.Point(101, 255);
+            this.btn_down.Name = "btn_down";
+            this.btn_down.Size = new System.Drawing.Size(75, 23);
+            this.btn_down.TabIndex = 1;
+            this.btn_down.Text = "DOWN";
+            this.btn_down.UseVisualStyleBackColor = true;
+            this.btn_down.Click += new System.EventHandler(this.btn_down_Click);
+            // 
+            // btn_left
+            // 
+            this.btn_left.Location = new System.Drawing.Point(20, 236);
+            this.btn_left.Name = "btn_left";
+            this.btn_left.Size = new System.Drawing.Size(75, 23);
+            this.btn_left.TabIndex = 2;
+            this.btn_left.Text = "LEFT";
+            this.btn_left.UseVisualStyleBackColor = true;
+            this.btn_left.Click += new System.EventHandler(this.btn_left_Click);
+            // 
+            // btn_right
+            // 
+            this.btn_right.Location = new System.Drawing.Point(182, 236);
+            this.btn_right.Name = "btn_right";
+            this.btn_right.Size = new System.Drawing.Size(75, 23);
+            this.btn_right.TabIndex = 3;
+            this.btn_right.Text = "RIGHT";
+            this.btn_right.UseVisualStyleBackColor = true;
+            this.btn_right.Click += new System.EventHandler(this.btn_right_Click);
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(284, 196);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(75, 23);
+            this.btn_select.TabIndex = 4;
+            this.btn_select.Text = "SELECT";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(284, 225);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 5;
+            this.btn_reset.Text = "RESET";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // tb_top
+            // 
+            this.tb_top.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_top.Location = new System.Drawing.Point(51, 68);
+            this.tb_top.MaxLength = 16;
+            this.tb_top.Name = "tb_top";
+            this.tb_top.Size = new System.Drawing.Size(186, 29);
+            this.tb_top.TabIndex = 6;
+            // 
+            // tb_bottom
+            // 
+            this.tb_bottom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_bottom.Location = new System.Drawing.Point(51, 103);
+            this.tb_bottom.MaxLength = 16;
+            this.tb_bottom.Name = "tb_bottom";
+            this.tb_bottom.Size = new System.Drawing.Size(186, 29);
+            this.tb_bottom.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 293);
+            this.Controls.Add(this.tb_bottom);
+            this.Controls.Add(this.tb_top);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.btn_right);
+            this.Controls.Add(this.btn_left);
+            this.Controls.Add(this.btn_down);
+            this.Controls.Add(this.btn_up);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_up;
+        private System.Windows.Forms.Button btn_down;
+        private System.Windows.Forms.Button btn_left;
+        private System.Windows.Forms.Button btn_right;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.TextBox tb_top;
+        private System.Windows.Forms.TextBox tb_bottom;
     }
 }
 
--- ArduinoMenuSystem/Form1.cs
+++ ArduinoMenuSystem/Form1.cs
@@ -16,5 +16,40 @@ namespace ArduinoMenuSystem
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_left_Click(object sender, EventArgs e)
+        {
+            //left button clicked
+        }
+
+        private void btn_up_Click(object sender, EventArgs e)
+        {
+            //up button clicked
+        }
+
+        private void btn_down_Click(object sender, EventArgs e)
+        {
+            //down button clicked
+        }
+
+        private void btn_right_Click(object sender, EventArgs e)
+        {
+            //right button clicked
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            //select button clicked
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            //reset button clicked
+        }
     }
 }
